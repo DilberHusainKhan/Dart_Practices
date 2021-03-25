2391478b6461f2388f12e76a00c36710b7c39f13
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-26 00:23:45 +0530

    variables and datatypes

--- variable.dart
+++ variable.dart
@@ -1,4 +1,29 @@
 void main() {
   var myVariable = 32;
   print(myVariable);
+
+  // INTEGER datatype
+  int numb = 210;
+  print(numb);
+
+  // int pie = 3.14;
+  // print(pie);
+
+  // DOUBLE datatype
+  double pi = 3.14;
+  print(pi);
+  double numbe = 2;
+  print(numbe); // output is 2.0
+
+  // NUM Datatype
+  num myAge = 24;
+  num myAgeWithMonth = 24.6;
+  print(myAge);
+  print(myAgeWithMonth);
+
+  // method to find the type of datatype
+  // runtimetype
+  var name = 'dilber';
+  print(name.runtimeType);
+  print(myAgeWithMonth.runtimeType);
 }
