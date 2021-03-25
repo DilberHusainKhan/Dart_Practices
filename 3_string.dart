6d6f1529f5b64a09a20a79010205486b95acd3fd
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-26 00:48:15 +0530

    String Basic

--- 3_string.dart
+++ 3_string.dart
@@ -1,1 +1,12 @@
+// String is a datatype that contain text.
+void main() {
+  String myName = "Dilber Husain Khan";
+  String myQue = "How's the food?";
 
+  print(myQue);
+  // Convert String to int.
+  String myAge = "24";
+  int x = int.parse(myAge);
+  print(x);
+  print(x.runtimeType);
+}
