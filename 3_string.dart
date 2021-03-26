c498ddabe3a393945d4b74f4e4b68f03939b79c8
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-27 00:21:47 +0530

    convert int ot String

--- 3_string.dart
+++ 3_string.dart
@@ -2,11 +2,17 @@
 void main() {
   String myName = "Dilber Husain Khan";
   String myQue = "How's the food?";
-
   print(myQue);
+
   // Convert String to int.
   String myAge = "24";
   int x = int.parse(myAge);
   print(x);
   print(x.runtimeType);
+
+  // Convert int to String .
+  int myCurrentAge = 24;
+  String myNowAge = myCurrentAge.toString();
+  print(myNowAge);
+  print(myNowAge.runtimeType);
 }
