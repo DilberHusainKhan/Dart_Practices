3349d3ef265ca1160a1fdb46731dc1d38d60e4b6
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-27 12:34:00 +0530

    boolean variable in Dart

---
+++ 4_booleans.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  bool isTrue = true;
+  /* bool isFalse = 2;
+   Error: A value of type 'int' can't be assigned to a variable of type 'bool'.
+
+  bool isFalse = 'hii';
+  Error: A value of type 'String' can't be assigned to a variable of type 'bool'.
+  */
+  bool isFalse = false;
+  print(isTrue);
+  print(isFalse);
+}
