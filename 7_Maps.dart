0f9bd650d7bdf5bc11a609f2b545a9b9de0bea91
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-29 01:13:28 +0530

    Map in dart

--- 7_Maps.dart
+++ 7_Maps.dart
@@ -1,2 +1,19 @@
 // Map it is like a dictionary.
-void main() {}
+// Key can be A string or number.
+void main() {
+  Map fruits = {1: 'Apple', 2: 'Banana', 3: 'Mango'};
+  print(fruits[2]);
+  Map myData = {
+    'name': 'Humayun Anwar Khan',
+    'age': 23,
+    'Mobile No': 8527462472
+  };
+  print(myData);
+  // To print keys of maps
+  print(myData.keys);
+  // To print value of map
+  print(myData.values);
+  // to print value using key
+  print(myData['name']);
+  print(myData.runtimeType);
+}
