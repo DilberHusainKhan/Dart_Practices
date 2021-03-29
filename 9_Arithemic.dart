9f62ae719348e2241aadb482ef9e8b69ceff7cb7
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-29 21:20:09 +0530

    Arithmetic operation

---
+++ 9_Arithemic.dart
@@ -0,0 +1,12 @@
+// Arithemtic operator
+
+void main() {
+  int age = 32;
+  double pie = 3.14;
+  print(age + pie);
+  print(age - pie);
+  print(age * pie);
+  print(age / pie);
+  print(age % pie);
+  print(5 - 4 + 10 * 10 / 10 + 10 / 100 * 123);
+}
