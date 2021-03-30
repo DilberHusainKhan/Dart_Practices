7e6be8209a80c32de3de48c9e603e9ac2198817e
Dilber Husain Khan <dilberjmi@gmail.com>
2021-03-31 01:37:34 +0530

    MAKE A ROBOT FOR FUN

---
+++ 12_robot.dart
@@ -0,0 +1,23 @@
+// make a Robot that buy milk from shop
+
+import 'dart:io';
+
+void main() {
+  // int bottle = 0, money = 0;
+  // print("Enter number of Milk bottle you want to buy");
+  // bottle = stdin.readByteSync();
+  // print("Enter money");
+  // money = stdin.readByteSync();
+  buyMilk(10, 2);
+}
+
+buyMilk(int moneyGiven, int bottle) {
+  int moneyToPaid = bottle * 2;
+  int moneyLeft = moneyGiven - moneyToPaid;
+  print("Given money $moneyGiven");
+  print("Go Out\nGTake Left\nGo to grocery shop");
+  print("Buy $bottle of Milk");
+  print("Pay $moneyToPaid for $bottle.");
+  print("Come Back to home");
+  print("You change $moneyLeft");
+}
