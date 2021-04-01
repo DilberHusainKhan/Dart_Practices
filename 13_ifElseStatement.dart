14b804c138c353bd72a44680167d139c0ac59b78
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-01 23:47:47 +0530

    Condition statement

--- 13_ifElseStatement.dart
+++ 13_ifElseStatement.dart
@@ -3,6 +3,8 @@
 void main() {
   // There are  three type of equality operator
   // as , is  , is!(is not)
-  String number = '2';
-  print(number is String);
+  int number = 2;
+  print(number is String); //output is False because it is int.
+  num number1 = 2;
+  print(number1 is int); // Output is True because num treate number as int.
 }
