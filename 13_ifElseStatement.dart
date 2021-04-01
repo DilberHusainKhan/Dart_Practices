17c20ecd75d6e92c0e5d15dfd712797b168ce7cc
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-01 23:54:16 +0530

    if else condition

--- 13_ifElseStatement.dart
+++ 13_ifElseStatement.dart
@@ -8,4 +8,10 @@ void main() {
   num number1 = 2;
   print(number1 is int); // Output is True because num treate number as int.
   print(number1 is! int); // Output is False because num treate number as int.
+  String myPassword = '12345678';
+  if (myPassword.length == 8) {
+    print('Password is accepted');
+  } else {
+    print('Password is rejected');
+  }
 }
