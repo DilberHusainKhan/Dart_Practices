fa0693511322d8a4d49dd8cfb21545df6be17a06
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-16 23:43:29 +0530

    Return Type in function

---
+++ 17_Retrning_Types_inFunction.dart
@@ -0,0 +1,20 @@
+// void is function which return nothing
+
+void main() {
+  print(getName());
+  print(age());
+}
+
+// String function return string
+String getName() => "Dilber Husain Khan";
+
+// Bool function Return Boolean value
+
+bool age() {
+  int myAge = 23;
+  if (myAge >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
