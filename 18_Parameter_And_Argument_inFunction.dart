4751c750a7e0d3ad3d183e75bfe4a83edcf722bc
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-17 22:47:09 +0530

    Parameter and argument in function

---
+++ 18_Parameter_And_Argument_inFunction.dart
@@ -0,0 +1,9 @@
+// Parameter and argument in function
+
+void main() {
+  print(sayHello("Humayun Anwar Khan"));
+  String result = sayHello("Dilber");
+  print(result);
+}
+
+String sayHello(String name) => "Hello $name";
