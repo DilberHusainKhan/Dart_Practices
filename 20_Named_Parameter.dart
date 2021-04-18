076186b99695c953e485cc1fde2cea893602c888
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-18 14:48:59 +0530

    Named

--- 20_Named_Parameter.dart
+++ 20_Named_Parameter.dart
@@ -1,17 +1,18 @@
 // Parameter is divided into two sub catagories a:> rquired , B:> optional
 // Optional is also divided into three part A:> Positional, B:> NAMED,  C:> Default.
-// OPTIONAL ---> means may or maynot give the value but we will have to make varaible in the
-// [] square bracket example in age  '?' used for null safety
-// POSITIONAL PARAMETER
+
+// NAMED PARAMETER
+// we used { } bracket
+// we can change the places of parameter
 
 void main() {
-  myInfo("Dilber", "Husain", 23);
-  myInfo("Humayun", "Anwar khan");
+  myInfo("Dilber", age: 23, lastName: "Husain");
+  myInfo("Humayun", lastName: "Anwar khan");
   myInfo("Humayun");
 }
 
 // we will have to write parameter same as we mention in method
-myInfo(String firstName, [String? lastName, int? age]) {
+myInfo(String? firstName, {String? lastName, int? age}) {
   print("Your name is $firstName $lastName");
   print("Your age is $age");
 }
