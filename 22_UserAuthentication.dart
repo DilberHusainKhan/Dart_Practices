4d6799aa10081a34edb324237931a028a4617baa
Dilber Husain Khan <dilberjmi@gmail.com>
2021-06-04 11:11:28 +0530

    User Authentication program in ---> CLI

--- 22_UserAuthentication.dart
+++ 22_UserAuthentication.dart
@@ -1,15 +1,26 @@
 import 'dart:io';
 
 void main() {
+  String? cho;
   String? email;
   String? password;
+  String? password1;
+  print("If you are already register press 1 else press 2");
+  cho = stdin.readLineSync();
+  if (cho == '1') {
+    userLogin(email, password, password1);
+  } else {
+    creatAccount(email, password, password1);
+  }
+}
+
+userLogin(String? email, String? password, String? password2) {
   print("Eneter email :- ");
   email = stdin.readLineSync();
   print("Enter Password:- ");
   password = stdin.readLineSync();
   if (email!.isEmpty && password!.isEmpty) {
     print("You will have to creat an account");
-    creatAccount(email, password);
   } else if (email.contains('@') &&
       email.contains('.com') &&
       password!.length >= 8) {
@@ -19,12 +30,17 @@ void main() {
   }
 }
 
-creatAccount(String? email, String? password) {
+creatAccount(String? email, String? pass, String? pass1) {
   print("Eneter email :- ");
   email = stdin.readLineSync();
   print("Enter Password:- ");
-  password = stdin.readLineSync();
-  if (email!.contains('@') && email.contains('.com') && password!.length >= 8) {
+  pass = stdin.readLineSync();
+  print("Confirm Password:- ");
+  pass1 = stdin.readLineSync();
+  if (email!.contains('@') &&
+      email.contains('.com') &&
+      pass!.length >= 8 &&
+      pass == pass1) {
     print("Login sucessful $email");
   } else {
     print("invail format");
