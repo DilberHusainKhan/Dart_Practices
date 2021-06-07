8f679020ae161ee3d79e2a9aa6116bdc2b66dbb2
Dilber Husain Khan <dilberjmi@gmail.com>
2021-06-07 09:38:47 +0530

    info about class and objects

---
+++ 23_Creating_classes_and_object.dart
@@ -0,0 +1,23 @@
+void main() {
+  var myCar = Car(); //Object creation
+  myCar.carName = 'Audi';
+  myCar.color = 'Black';
+  myCar.varient = 1;
+  print(myCar.carName);
+  print(myCar.color);
+  print(myCar.varient);
+
+  var myCar2 = new Car(); //new second object
+  myCar2.color = 'White';
+
+  print(myCar2.carName); //override the value
+  print(myCar2.color);
+  print(myCar2.varient);
+}
+
+// Class
+class Car {
+  String? carName = "My car";
+  String? color;
+  int? varient;
+}
