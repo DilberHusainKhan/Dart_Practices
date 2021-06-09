743b5ea7155a0dbbb644c894a359728887db0bdc
Dilber Husain Khan <dilberjmi@gmail.com>
2021-06-09 14:00:50 +0530

    Concept of Constructor

---
+++ 25_constructors.dart
@@ -0,0 +1,20 @@
+void main() {
+  var myCar = new Car("Audii", "black", 4);
+  print("${myCar.carName} , ${myCar.color} , ${myCar.varient}");
+
+  print("-------------New Object ---------------");
+  var myCar2 = new Car("BMW", "Red", 1);
+  print("${myCar2.carName} , ${myCar2.color} , ${myCar2.varient}");
+}
+
+class Car {
+  String? carName;
+  String? color;
+  int? varient;
+// Constructor
+  Car(String carName, String color, int varient) {
+    this.carName = carName;
+    this.color = color;
+    this.varient = varient;
+  }
+}
