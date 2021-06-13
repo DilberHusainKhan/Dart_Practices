93e272324b52e281872bbad7414dc679362773ba
Dilber Husain Khan <dilberjmi@gmail.com>
2021-06-13 13:26:11 +0530

    Named constructor in dart

--- 26_Named_and_Syntactic_Constructor.dart
+++ 26_Named_and_Syntactic_Constructor.dart
@@ -1,1 +1,45 @@
+import '23_Creating_classes_and_object.dart';
 
+void main() {
+  print("Default conatyructor");
+  var myCar = new Car("Default", "cyan ", 0);
+  print(
+      "Default Constructor ${myCar.carName} , ${myCar.color}, ${myCar.varient}");
+  var myCar1 = new Car.Varient1();
+  print("*************************");
+  print("${myCar1.carName} \n${myCar1.color}  \n${myCar1.varient}");
+
+  var myCar2 = new Car.Varient2();
+  print("*************************");
+  print("${myCar2.carName} \n${myCar2.color}  \n${myCar2.varient}");
+
+  var myCar3 = new Car.Varient3();
+  print("*************************");
+  print("${myCar3.carName} \n${myCar3.color}  \n${myCar3.varient}");
+}
+
+class Car {
+  String? carName;
+  String? color;
+  int? varient;
+
+// Default construvtor
+  Car(this.carName, this.color, this.varient);
+
+// Named Constructor
+  Car.Varient1() {
+    carName = "ASBA";
+    color = "Yellow";
+    varient = 1;
+  }
+  Car.Varient2() {
+    carName = "ASBA";
+    color = "Red";
+    varient = 2;
+  }
+  Car.Varient3() {
+    carName = "ASBA";
+    color = "Black";
+    varient = 3;
+  }
+}
